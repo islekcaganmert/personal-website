83c127549ea8a47737450c9e167908dce7d1400b
islekcaganmert <islekcaganmert@hereus.net>
2024-05-19 23:10:50 +0300

    static and redirects

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/<path:path>', defaults={'path': ''})
-def catcher(path: str) -> str:
-    return path
+@app.route('/')
+def catcher() -> str:
+    return render_template('index.html')
