54426b9237f02f03e308f4d575fa64fbaed8cb5a
islekcaganmert <islekcaganmert@hereus.net>
2024-05-19 23:15:36 +0300

    webfinger redirect fix

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def catcher() -> str:
+def index() -> str:
     return render_template('index.html')
+
+
+@app.route('/.well-known/web-meta')
+def web_meta():
+    return redirect('https://web.brid.gy/.well-known/web-meta')
+
+
+@app.route('/.well-known/webfinger')
+def webfinger():
+    return redirect(f'https://web.brid.gy/.well-known/webfinger?resource={request.args['resource']}')
